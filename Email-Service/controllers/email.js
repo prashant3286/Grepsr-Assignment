e143a42e97a05b1c3ad3bb09bd3ad7fcdf01c4e8
Prashant Bhandari <cu.17bcs3286@gmail.com>
2023-02-27 08:26:25 +0545

    add message queue for sending email

--- Email-Service/controllers/email.js
+++ Email-Service/controllers/email.js
@@ -7,6 +7,11 @@ exports.sendEmail = async (req, res) => {
     const now = new Date()
     const email = new Email({...req.body, sentDate: now.toISOString().split('T')[0], status: "SENT"})
 
+     // send message to queue
+     instance = await broker.getInstance()
+     instance.send('email_queue', Buffer.from(JSON.stringify(email)))
+ 
+
     //save to db
     email.save((err, data) => {
         if (err) {
